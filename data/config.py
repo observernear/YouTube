5c1dac6cc8733b6837a33055608bb4d0003bb953
7KaZaX7 <lapinikita@mail.ru>
2024-05-30 11:13:36 +0300

    first_commit

---
+++ data/config.py
@@ -0,0 +1,24 @@
+# api id, hash
+API_ID = 2313
+API_HASH = '342ecass'
+
+# задержка между подключениями к аккаунтам
+ACC_DELAY = [1, 15]
+
+# папка с сессиями (не менять)
+WORKDIR = "sessions/"
+
+# использование прокси
+USE_PROXY = False # True/False
+
+# полный путь к вашей папке sessions 
+SESSIONS_PATH = "/home/anonim/project/Blum/sessions/"
+
+POINTS = [240, 300] #[min, max]
+
+SPEND_DIAMONDS = True
+
+# сон между играми
+SLEEP_GAME_TIME = [50,100] #[min,max]
+
+hello ='''Пора играть!'''
