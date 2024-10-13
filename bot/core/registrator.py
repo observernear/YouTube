93cd7fa804af6f4ebdcfdb75fd150e2ca423217d
observernear <lapinikita@mail.ru>
2024-10-13 22:40:31 +0300

    fix bug

--- bot/core/registrator.py
+++ bot/core/registrator.py
@@ -1,34 +1,24 @@
 from pyrogram import Client
 
 from bot.config import settings
-<<<<<<< HEAD
 from bot.utils import logger
-=======
 from bot.core.agents import generate_random_user_agent
 from bot.utils import logger
 from bot.utils.file_manager import save_to_json
 
->>>>>>> 25aba28 (edit proxy)
 
 async def register_sessions() -> None:
     API_ID = settings.API_ID
     API_HASH = settings.API_HASH
 
     if not API_ID or not API_HASH:
-<<<<<<< HEAD
-        raise ValueError("API_ID and API_HASH not found in the .env file.")
-
-    session_name = input('\nEnter the session name (press Enter to exit): ')
-=======
         raise ValueError("API_ID и API_HASH не найдены ")
 
     session_name = input('\nВведи название сессии, желательно на английском (нажми Enter чтобы выйти): ')
->>>>>>> 25aba28 (edit proxy)
 
     if not session_name:
         return None
 
-<<<<<<< HEAD
     session = Client(
         name=session_name,
         api_id=API_ID,
@@ -40,7 +30,7 @@ async def register_sessions() -> None:
         user_data = await session.get_me()
 
     logger.success(f'Session added successfully @{user_data.username} | {user_data.first_name} {user_data.last_name}')
-=======
+
     raw_proxy = input("Введи прокси в формате type://user:pass:ip:port (нажми Enter чтобы продолжить без прокси): ")
     session = await get_tg_client(session_name=session_name, proxy=raw_proxy)
     async with session:
@@ -81,4 +71,3 @@ async def get_tg_client(session_name: str, proxy: str | None) -> Client:
     )
 
     return tg_client
->>>>>>> 25aba28 (edit proxy)
